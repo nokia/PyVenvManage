e9514789bafcc3aa61dc2ea26a926c29d0085344
Andras Kovi <akovi@nokia.com>
2019-10-25 07:58:28 +0200

    Update versions and minor tweaks

---
+++ example-project/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ExamplePackageForPyVenvManage',
+    version='1.0.0',
+    url='https://github.com/nokia/PyVenvManage',
+    author='Andras Kovi',
+    author_email='akovi@nokia.com',
+    description='Use this for testing the plugin',
+    packages=find_packages(),
+)
